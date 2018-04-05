c1301637a8ab84c3bf4bb41d2ebafaa05749c9fc
Artem Klepcha <woullbors96@yandex.ru>
2018-04-05 17:05:40 +0300

    даваааай

---
+++ proj/Assets/ButtonScr.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScr : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+    public void Test()
+    {
+        Debug.Log(this.tag);
+    }
+
+
+	// Update is called once per frame
+	void Update () {
+	}
+}
---
+++ proj/Assets/Code/ChangeColor.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeColor : MonoBehaviour {
+
+	// Use this for initialization
+    public Color cColor;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void SetColor(string color)
+    {
+        switch (color)
+        {
+            case "red": GetComponent<Renderer>().material.color = Color.red; break;
+            case "green": GetComponent<Renderer>().material.color = Color.green; break;
+            case "blue": GetComponent<Renderer>().material.color = Color.blue; break;
+        }
+    }
+}
